a046c45b557e870dc3f39d5934d07d3f3bedead5
Mohammad Ali <aliawan87@gmail.com>
2025-08-25 10:20:44 +0500

    Adding Validations

--- todo.rb
+++ todo.rb
@@ -1,7 +1,42 @@
 require "sinatra"
 require "sinatra/reloader"
-require "tilt/erubis"
+require "erubis"
+require "securerandom"
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/lists" do
+  @lists = session[:lists]
+
+  erb :lists
+end
+
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  if list_name.size.between?(1,100)
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The new list was created successfully!"
+    redirect "/lists"
+  else
+    session[:failure] = "The name should between 1 and 100 Characters."
+    redirect "/lists/new"
+  end
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
 
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
 end
+
+ 
